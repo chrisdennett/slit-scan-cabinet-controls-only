a8b4ed2d75c9c9c7ca10ec7c32a2947b8a9a7937
chrisdennett <chrisdennett@gmail.com>
2022-01-27 20:36:06 +0000

    Flipped vertically as well to work with upsidedown webcam mouting

--- app/js/utils/getFlippedVideoCanvas.js
+++ app/js/utils/getFlippedVideoCanvas.js
@@ -3,8 +3,8 @@ export function getFlippedVideoCanvas(video) {
   frameCanvas.width = 320;
   frameCanvas.height = 240;
   const frameCtx = frameCanvas.getContext("2d");
-  frameCtx.translate(frameCanvas.width, 0);
-  frameCtx.scale(-1, 1);
+  frameCtx.translate(frameCanvas.width, frameCanvas.height);
+  frameCtx.scale(-1, -1);
   frameCtx.drawImage(video, 0, 0);
   return frameCanvas;
 }
