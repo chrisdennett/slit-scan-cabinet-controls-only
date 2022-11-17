491bd17e19ecf19c45d6e9e5c69c73696432b1e5
Chris Dennett <chrisdennett@gmail.com>
2022-11-17 15:12:27 +0000

    Tidied up the code and added comments for rotary encoder

--- webserver.js
+++ webserver.js
@@ -16,30 +16,27 @@ server.listen(3000, () => {
 });
 
 const Gpio = require("onoff").Gpio;
-const button = new Gpio(4, "in", "rising");
-const button2 = new Gpio(24, "in", "rising");
 
+// connect + from dial to any power pin e.g. pin number 01
+// connect grd from dial to any ground pin e.g. pin number 06
+// connect sw from dial to gpio 4
+const button = new Gpio(4, "in", "rising");
+
+// https://thepihut.com/blogs/raspberry-pi-tutorials/how-to-use-a-rotary-encoder-with-the-raspberry-pi
+// attach CLK to pin GPIO 17
+// DT to GPIO 18
 const nodaryEncoder = require("nodary-encoder");
-const myEncoder = nodaryEncoder(18, 17); // Using GPIO17 & GPIO18
-const myEncoder2 = nodaryEncoder(25, 23);
+const myEncoder = nodaryEncoder(18, 17);
 
 let prevValue = null;
 
 // button event
 button.watch((err, value) => io.emit("isReflected", { value: value }));
-button2.watch((err, value) => io.emit("doReset", { value: value }));
 
 // turn event
 myEncoder.on("rotation", (direction, value) => {
   if (value !== prevValue) {
     io.emit("sliceSizeChange", { value: direction });
-    prevValue = value;
-  }
-});
-
-myEncoder2.on("rotation", (direction, value) => {
-  if (value !== prevValue) {
-    io.emit("webcamPositionChange", { value: direction });
     prevValue = value;
   }
 });
